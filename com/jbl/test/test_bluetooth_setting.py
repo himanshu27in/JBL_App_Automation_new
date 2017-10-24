fb9c8db6a94573e41e40462fb60d53f582e0a711
himanshu <himanshu27in@gmail.com>
2017-10-24 13:59:59 +0530

    added test
    Change-Id: I958bfceb38f049be3d2a63e45b97289584c1af6b

--- com/jbl/test/test_bluetooth_setting.py
+++ com/jbl/test/test_bluetooth_setting.py
@@ -12,7 +12,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-
+from time import sleep
+import unittest
 
 '''
 This class is having all test cases related to bluetooth pairing,
@@ -20,6 +21,7 @@ bluetooth available devices, connecting and disconnecting via BT
 
 '''
 class Bluetooth_Connection_Setting(setupConfig):
+    
     
     def connect_headphone_via_bt(self):
         value = 1
@@ -34,8 +36,9 @@ class Bluetooth_Connection_Setting(setupConfig):
                 if headphoneObj:
                     logging.info("Headphone available for connection is :  " + headphoneObj.group())
                     device_name.click()
-                    self.driver.implicitly_wait(10000)
+                    sleep(5)
                     value = 0
+                    logging.info('Headphone is connected with device')
                     break
                 else:
                     flag = 'false'
@@ -54,23 +57,79 @@ class Bluetooth_Connection_Setting(setupConfig):
         connected_string = "Connected"
         connectedStringObj = re.search(connected_string, bt_connection_status.text, re.M | re.I)
         logging.info(connectedStringObj.group())
-        
+        flag = False
         if connectedStringObj.group() == connected_string:
             bt_connection_status.click()
-
+            sleep(1)
         disconnect_string= self.driver.find_element_by_id("android:id/alertTitle").text
         if disconnect_string=="Disconnect?":
             self.driver.find_element_by_id("android:id/button1").click()
             self.driver.implicitly_wait(10000)
             logging.info("Headphone is disconnected from previously connected device !")
-            assert True
+            flag = True
         else:
-            logging.info("Not able to find the disconnect pop up message window !")
-            assert False
+            logging.info("Not able to find the disconnect pop up message window ! or already disconnected")
+            flag = False
+        return flag
 
+    def headphone_already_connected(self):
+        bt_connection_status = self.driver.find_elements_by_class_name("android.widget.TextView")
+        
+        connected_to_headset = self.driver.find_element_by_id("android:id/title")
+        
+        flag = False
+        for status in bt_connection_status:
+            logging.info(status.text)
+            logging.info(connected_to_headset.text)
+            logging.info("==================")
+            if status.text[:9] == "Connected" and connected_to_headset.text == constants.JBL_Everest_Elite_750NC:
+                logging.info("Device is connected with JBL headphone")
+                flag = True
+                break
+        return flag
+        
+    def connect_headphone_when_paired_and_poweredoff(self):
+
+        bt_device_list=self.driver.find_element_by_id("android:id/title")
+        headphoneObj = re.match(constants.JBL_Everest_Elite_750NC, bt_device_list.text, re.M | re.I)
+        if headphoneObj:
+            logging.info("Headphone available for connection is :  " + headphoneObj.group())
+            bt_device_list.click()
+            sleep(3)
+            connection_verification = self.driver.find_element_by_id("android:id/summary").text
+            connection_verification = connection_verification[:10]
+            if connection_verification == "Connecting" or connection_verification == None:
+                logging.info("Headphone is powered off. Please switch it ON or taking long time to connect...")
+                return True
+        else:
+            logging.info("Headphone is not paired with device")
+            return False
+        
+    def unpair_connected_device(self):
+        bt_setting_button = self.driver.find_element_by_id("com.android.settings:id/deviceDetails")
+        bt_setting_button.click()
+        product_under_test = self.driver.find_element_by_id("com.android.settings:id/name")
+        
+        if product_under_test.text == constants.JBL_Everest_Elite_750NC:
+            self.driver.find_element_by_id("android:id/button3").click()
+            self.driver.implicitly_wait(10000)
+            logging.info("Headphone is unpaired with device !")
          
+        bt_available_list = self.driver.find_elements_by_class_name("android.widget.TextView")
+        flag = False
+        for bt_list in bt_available_list:
+            if bt_list.text == "Paired devices" and product_under_test.text == constants.JBL_Everest_Elite_750NC:
+                logging.info("Headphone is not unpaired properly")
+                flag = False
+                break
+            else:
+                flag = True
+        return flag     
+        
+        
+             
 
-
+    #@unittest.skip("test_01_connect_headphone_via_BT_in_pairing_state")
     def test_01_connect_headphone_via_BT_in_pairing_state(self):
         # To open Bluetooth settings screen on mobile
         self.driver.implicitly_wait(10000)
@@ -78,21 +137,25 @@ class Bluetooth_Connection_Setting(setupConfig):
         os.system("adb shell am start -n com.android.settings/.bluetooth.BluetoothSettings")
         #adb command for LG G5
         #os.system("adb shell am start -n com.lge.bluetoothsetting/.LGBluetoothSettingActivity")
-        #put headphone in Bt pairing state, check for headphone name in BT window of phone. 
         
+        #put headphone in Bt pairing state, check for headphone name in BT window of phone. 
+ 
         
         #if found the BT name on phone then click on it else scroll the BT window screen check for headphone name
         # new list.
         
         # connect headphone via BT 
-        self.connect_headphone_via_bt()
+        if self.headphone_already_connected() == False:
+            self.connect_headphone_via_bt()
        
         # To check whether headphone is powered on or not
         connection_verification = self.driver.find_element_by_id("android:id/summary").text
+
         #waiting for Headphone to get connected with phone
         try:
-            WebDriverWait(self.driver,5).until(EC.text_to_be_present_in_element((By.ID,'android:id/summary'),'Connected'))
+            WebDriverWait(self.driver,10).until(EC.text_to_be_present_in_element((By.ID,'android:id/summary'),'Connected'))
             logging.info("Headphone connected via BT... ")
+            
         except TimeoutException:
             connection_verification = connection_verification[:10]
             if connection_verification == "Connecting":
@@ -108,57 +171,68 @@ class Bluetooth_Connection_Setting(setupConfig):
             assert True
             logging.info("Headphone is Connected to device via BT")
         else:
-            logging.info("Unable to connect via BT")
+            logging.info("Unable to connect via BT or not able to capture connected string")
             assert False
         logging.info("going to call disconnect")
         self.disconnect_headphone_via_bt()   
         
-#     # this test is dependent on test 01         
-#     def test_02_disconnect_already_connected_headphone(self):
-#         # To open Bluetooth settings screen on mobile
-#         self.driver.implicitly_wait(10000)
-#         # adb command for sony xperia
-#         os.system("adb shell am start -n com.android.settings/.bluetooth.BluetoothSettings")
-#          
-#          
-#         connection_verification = self.driver.find_element_by_id("android:id/summary").text
-#         if connection_verification == None:
-#              
-#              
-#         
-#         
-#         
-#         
-#         
-#         
-#         # To check whether headphone is powered on or not
-#         connection_verification = self.driver.find_element_by_id("android:id/summary").text
-#         connection_verification = connection_verification[:10]
-#         if connection_verification == "Connecting":
-#             logging.info("Headphone is powered off. please switch it ON...")
-#             assert False
-#         #waiting for Headphone to get connected with phone
-#         try:
-#             WebDriverWait(self.driver,5).until(EC.text_to_be_present_in_element((By.ID,'android:id/summary'),'Connected'))
-#             logging.info("Headphone connected via BT... ")
-#         except TimeoutException:
-#             logging.info("time ran out...")
-#             assert False
-#         # validating connected string on Phone Bluetooth setting window  
-#         check_string="Connected"
-#         searchObj = re.search(check_string, connection_verification, re.M | re.I)
-#         
-#         logging.info("Comparing 'Connected' string with : " + searchObj.group())
-#         if searchObj.group() == "Connected":
-#             assert True
-#             logging.info("Headphone is Connected to device via BT")
-#         else:
-#             logging.info("Unable to connect via BT")
-#             assert False
+      
+    def test_02_disconnect_already_connected_headphone(self):
+        # To open Bluetooth settings screen on mobile
+        self.driver.implicitly_wait(10000)
+        # adb command for sony xperia
+        os.system("adb shell am start -n com.android.settings/.bluetooth.BluetoothSettings")
+        #First check headphone is connected or not, if not connected then display the message as headphone is in BT disconnected mode. otherwise performing BT disconnect operation
+        # check whether device disconnected properly or not.
+    
+        if self.headphone_already_connected() == False :
+            logging.info("Headphone is in BT disconnected mode, please connect the headphone via BT....!!!")
+            self.connect_headphone_via_bt()
+        else:
+            self.disconnect_headphone_via_bt()
+            self.driver.implicitly_wait(10000)
+            if self.headphone_already_connected() == False:
+                logging.info("device proper disconnected from previously connected device .....")
+            else:
+                logging.info(" device still in connected state after BT disconnect operation ")
+                assert False
 
 
 
 
+    def test_03_reboot_headphone_to_validate_connected_to_previously_connected_device(self):
+        
+        logging.info("Executing test_03_reboot_headphone_to_validate_connected_to_previously_connected_device")
+        self.driver.implicitly_wait(10000)
+        # adb command for sony xperia
+        os.system("adb shell am start -n com.android.settings/.bluetooth.BluetoothSettings")
 
+        #Headphone is already paired and connected to any mobile
+        check_connected = self.headphone_already_connected()
+        if check_connected == True:
+            logging.info("Headphone already connected")
+        else:
+            self.connect_headphone_via_bt()
+        
+        #Power off and power ON headphone
+        
+        # validate it is connected to previously connected device 
+        
+        check_connected = self.headphone_already_connected()
+        assert check_connected        
 
+    
+    def test_04_unpair_connected_device(self):
+        self.driver.implicitly_wait(10000)
+        # adb command for sony xperia
+        os.system("adb shell am start -n com.android.settings/.bluetooth.BluetoothSettings")
 
+        #Headphone is already paired and connected to any mobile
+        check_connected = self.headphone_already_connected()
+        if check_connected == True:
+            logging.info("Headphone already connected")
+            # unpair it
+            unpair_status = self.unpair_connected_device()
+            assert unpair_status
+            
+            
